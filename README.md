a5b4a717865d2c343d347b86d60cb9e6aed3f313
John Lyon-Smith <john@lyon-smith.org>
2025-05-27 18:48:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prusaconnect Uploader
+
+[![coverage](https://shields.io/endpoint?url=https://raw.githubusercontent.com/jlyonsmith/prusaconnect_uploader/main/coverage.json)](https://github.com/jlyonsmith/prusaconnect_uploader/blob/main/coverage.json)
+[![Crates.io](https://img.shields.io/crates/v/prusaconnect_uploader.svg)](https://crates.io/crates/prusaconnect_uploader)
+[![Docs.rs](https://docs.rs/prusaconnect_uploader/badge.svg)](https://docs.rs/prusaconnect_uploader)
+
+Camera still uploader for Prusa Connect
---
+++ src/bin/prusaconnect_uploader.rs
@@ -0,0 +1,32 @@
+use core::fmt::Arguments;
+use prusaconnect_uploader::{error, PrusaconnectUploaderLog, PrusaconnectUploaderTool};
+use yansi::Paint;
+
+struct PrusaconnectUploaderLogger;
+
+impl PrusaconnectUploaderLogger {
+    fn new() -> PrusaconnectUploaderLogger {
+        PrusaconnectUploaderLogger {}
+    }
+}
+
+impl PrusaconnectUploaderLog for PrusaconnectUploaderLogger {
+    fn output(self: &Self, args: Arguments) {
+        println!("{}", args);
+    }
+    fn warning(self: &Self, args: Arguments) {
+        eprintln!("{}", Paint::yellow(&format!("warning: {}", args)));
+    }
+    fn error(self: &Self, args: Arguments) {
+        eprintln!("{}", Paint::red(&format!("error: {}", args)));
+    }
+}
+
+fn main() {
+    let logger = PrusaconnectUploaderLogger::new();
+
+    if let Err(error) = PrusaconnectUploaderTool::new(&logger).run(std::env::args_os()) {
+        error!(logger, "{}", error);
+        std::process::exit(1);
+    }
+}
